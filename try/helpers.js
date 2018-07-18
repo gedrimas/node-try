60ecbd3bd68e501d72357c66c5e954640556bfb5
gedrimas <a.a.gedrimas@gmail.com>
2018-07-18 19:32:16 +0300

    first commit

---
+++ try/helpers.js
@@ -0,0 +1,6 @@
+
+const obj = require('./lib');
+
+console.log(obj.cal(3, 5));
+console.log(obj.someProperty);
+console.log(obj);
---
+++ user/index.js
@@ -0,0 +1,12 @@
+var db = require('../db');
+
+
+function User(name){
+    this.name = name;
+}
+
+User.prototype.hello = function(who){
+    console.log(db.getPhrase("Hello") + ' ' + who.name);
+};
+
+module.exports = User;
